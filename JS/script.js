6199db59f595e83a23d375ea64c70f249cfb5c46
Daniel Morales Osorio <Daniem976@gmail.com>
2025-06-24 23:05:21 -0700

    attack implented

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
-var btnfx = document.getElementById("btnfx");
-let mbgm = document.getElementById("main_bgm")
-function playAudio(Audio) 
-{ 
-  Audio.play();
+//This Section Covers Attack Damage//
+let enemyhealth = 100;
+setInterval(function(){
+    let healthvar = document.getElementById("health");
+    healthvar.value = enemyhealth;
+
+    if (enemyhealth < .01){ 
+        enemyhealth = .1;
+        alert("you win")
+    }
+}, 1);
+
+function attackenemy(){
+    let damage = Math.floor(Math.random() * 10 + 1);//Random Damage Value Btwn : 10-1 //
+    enemyhealth -= damage;
 }
