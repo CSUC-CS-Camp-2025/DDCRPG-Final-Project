f6ffb836b3c31ab99b77e8ba43d23974565c9313
Kernier <157945663+Kernier@users.noreply.github.com>
2025-06-25 09:02:34 -0700

    added intro dialogue

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,24 @@ function playAudio(Audio)
 { 
   Audio.play();
 }
+
+const dialogue = [
+  "Hi, Grandma!"
+  ,"Hey, Kid! I need help!"
+  ,"There's monsters stealing my sheep!"
+  ,"Could you help your dear grandmother out?"
+  ,"Of course Grandma!"
+]
+
+let talking = 0 
+function conversation() {
+  talking = talking + 1;
+  document.getElementById("text").textContent = dialogue[talking];
+  if (talking == dialogue.length){
+    talking = 0;
+  };
+}
+
+document.getElementById("nextButton").addEventListener('click', () =>{
+  conversation();
+});
