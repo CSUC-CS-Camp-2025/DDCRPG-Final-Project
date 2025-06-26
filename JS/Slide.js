ad580525e6fb1543e18ab186670a980135f1b634
Daniel Morales Osorio <Daniem976@gmail.com>
2025-06-26 01:54:49 -0700

    added sprites to all cutscenes-could not get protag choice;

--- JS/Slide.js
+++ JS/Slide.js
@@ -1,1 +1,8 @@
 const swup = new Swup();
+let maleMC = "/gallery/maleMC.png";
+let femaleMC = "/gallery/femaleMC.png";
+let MCgender;
+function Charsect(protag){
+    let MCgender = protag
+    console.log(MCgender)
+}
